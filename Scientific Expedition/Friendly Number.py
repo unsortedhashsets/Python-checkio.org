bc1b44d2193c60ce0948b3db33f591435e02a611
Mikhail Abramov <46084942+unsortedhashsets@users.noreply.github.com>
2018-12-28 13:27:58 +0100

    Scientific Expedition - Friendly Number
    n=102
    result: "102", the base is default 1000 and 102 is lower this base.
    n=10240
    result: "10k", the base is default 1000 and rounding down.
    n=12341234, decimals=1
    result: "12.3M", one digit after the dot.
    n=12000000, decimals=3
    result: "12.000M", trailing zeros.
    n=12461, decimals=1
    result: "12.5k", standard rounding.
    n=1024000000, base=1024, suffix='iB'
    result: '976MiB', the different base and the suffix.
    n=-150, base=100, powers=['', 'd', 'D']
    result: '-1d', the negative number and rounding towards zero.
    n=-155, base=100, decimals=1, powers=['', 'd', 'D']
    result: '-1.6d', the negative number and standard rounding.
    n=255000000000, powers=['', 'k', 'M']
    result: '255000M', there is not enough powers.
    Input: A number as an integer. The keyword argument "base" as an integer, default 1000. The keyword argument "decimals" as an integer, default 0. The keyword argument "powers" as a list of string, default ['', 'k', 'M', 'G', 'T', 'P', 'E', 'Z', 'Y'].
    
    Output: The converted number as a string

---
+++ Scientific Expedition/Friendly Number.py
@@ -0,0 +1,17 @@
+def friendly_number(number, base=1000, decimals=0, suffix='',
+                    powers=['', 'k', 'M', 'G', 'T', 'P', 'E', 'Z', 'Y']):
+    power, number = 0, float(number)
+    while abs(number) >= base and power+1 < len(powers):
+        number /= base
+        power += 1
+    if decimals == 0:
+        number=int(number)
+    return '%.*f'%(decimals,number) + powers[power] + suffix
+
+#These "asserts" using only for self-checking and not necessary for auto-testing
+if __name__ == '__main__':
+    assert friendly_number(102) == '102', '102'
+    assert friendly_number(10240) == '10k', '10k'
+    assert friendly_number(12341234, decimals=1) == '12.3M', '12.3M'
+    assert friendly_number(12461, decimals=1) == '12.5k', '12.5k'
+    assert friendly_number(1024000000, base=1024, suffix='iB') == '976MiB', '976MiB'
